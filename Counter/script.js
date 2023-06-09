d4fe5b4e1159210c549d31ab1472db4896db556e
monic vyas <118365854+Monic-19@users.noreply.github.com>
2023-06-09 21:40:35 +0530

    Simple Counter

---
+++ Counter/script.js
@@ -0,0 +1,14 @@
+let count=document.querySelector(".counter")
+
+function increment(){
+let value=parseInt(count.innerText);
+value=value+1;
+count.innerText=value;
+}
+
+
+function decrement(){
+    let value=parseInt(count.innerText);
+    value=value-1;
+    count.innerText=value;
+    }
